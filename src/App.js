6027f5b5a03844584ad29823503976e0e6938bdd
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-23 21:05:31 -0800

    Adds basic Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupsPage from './pages/NewMeetup';
+
 function App() {
-  return (<div>test</div>);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetups" element={<NewMeetupsPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
