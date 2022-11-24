225e9fc3811be633439467d6ddb63cbbc4ec8a34
Omar Ochoa <omar@ochoawebdesign.com>
2022-11-23 21:11:41 -0800

    Creates MainNavigation component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupsPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetups" element={<NewMeetupsPage />} />
